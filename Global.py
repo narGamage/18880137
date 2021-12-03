1758c97d7ee4e2bb4d5b8998aa0a7e82c6e6303d
narGamage <narmadadg@gmail.com>
2021-12-04 01:04:38 +0530

    first file upload

---
+++ Global.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 11 22:43:59 2021
+
+@author: wenura jayadewa
+"""
+
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import numpy as np
+import pandas as pd
+
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+    
+df = pd.read_csv("Global.csv")
+print(df.head())
+df.fillna(999, inplace=True)
+X = df.drop(labels=['cardio'],axis=1)
+y = df['cardio']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+sss=X_train
+
+
+
+from sklearn.decomposition import PCA
+
+pca = PCA()
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+sss1=X_train
+
+
+explained_variance = pca.explained_variance_ratio_
+
+Find_Most_Sutable=abs( pca.components_ )
+
+
+from sklearn.decomposition import PCA
+ 
+pca = PCA(n_components=5)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+
+
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes  import GaussianNB
+
+
+models = []
+models.append(('LogisticRegression', LogisticRegression()))
+models.append(('DecisionTreeClassifier', DecisionTreeClassifier()))
+models.append(('GradientBoostingClassifier', GradientBoostingClassifier()))
+models.append(('KNeighborsClassifier', KNeighborsClassifier()))
+models.append(('RandomForestClassifier', RandomForestClassifier()))
+models.append(('Naive Bayes', GaussianNB()))
+
+
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=10, random_state=50,shuffle=True)
+    cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+   
+
+# this Logistic regression 
+model=LogisticRegression()
+model.fit(X_train,y_train)
+pred_y=model.predict(X_test)
+
+cm = metrics.confusion_matrix(y_test, pred_y)
+plt.figure(figsize=(9,9))
+sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
+plt.ylabel('Actual label');
+plt.xlabel('Predicted label');
+all_sample_title = 'LogisticRegression Confusion Matrix - score:'+str(metrics.accuracy_score(y_test,pred_y))
+plt.title(all_sample_title, size = 15);
+plt.show()
+print(metrics.classification_report(y_test,pred_y))   
+   
+    
+   
+# this DecissiontreeClassifier
+model=DecisionTreeClassifier()
+model.fit(X_train,y_train)
+pred_y=model.predict(X_test)
+
+cm = metrics.confusion_matrix(y_test, pred_y)
+plt.figure(figsize=(9,9))
+sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
+plt.ylabel('Actual label');
+plt.xlabel('Predicted label');
+all_sample_title = 'DecisionTreeClassifier Confusion Matrix - score:'+str(metrics.accuracy_score(y_test,pred_y))
+plt.title(all_sample_title, size = 15);
+plt.show()
+print(metrics.classification_report(y_test,pred_y))
+
+
+
+# this GradientBoostingClassifier
+model=GradientBoostingClassifier()
+model.fit(X_train,y_train)
+pred_y=model.predict(X_test)
+
+cm = metrics.confusion_matrix(y_test, pred_y)
+plt.figure(figsize=(9,9))
+sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
+plt.ylabel('Actual label');
+plt.xlabel('Predicted label');
+all_sample_title = 'GradientBoostingClassifier Confusion Matrix - score:'+str(metrics.accuracy_score(y_test,pred_y))
+plt.title(all_sample_title, size = 15);
+plt.show()
+print(metrics.classification_report(y_test,pred_y))
+
+
+# this KNeighborsClassifierClassifier
+model=KNeighborsClassifier()
+model.fit(X_train,y_train)
+pred_y=model.predict(X_test)
+
+cm = metrics.confusion_matrix(y_test, pred_y)
+plt.figure(figsize=(9,9))
+sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
+plt.ylabel('Actual label');
+plt.xlabel('Predicted label');
+all_sample_title = 'KNeighborsClassifier Confusion Matrix - score:'+str(metrics.accuracy_score(y_test,pred_y))
+plt.title(all_sample_title, size = 15);
+plt.show()
+print(metrics.classification_report(y_test,pred_y))
+
+    
+# this RandomForestClassifier
+model=RandomForestClassifier(n_estimators=1000)
+model.fit(X_train,y_train)
+pred_y=model.predict(X_test)
+
+
+
+cm = metrics.confusion_matrix(y_test, pred_y)
+plt.figure(figsize=(9,9))
+sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
+plt.ylabel('Actual label');
+plt.xlabel('Predicted label');
+all_sample_title = 'RandomForestClassifier Confusion Matrix - score:'+str(metrics.accuracy_score(y_test,pred_y))
+plt.title(all_sample_title, size = 15);
+plt.show()
+print(metrics.classification_report(y_test,pred_y))
+
+
+# this Naive BayesClassifier
+model=KNeighborsClassifier()
+model.fit(X_train,y_train)
+pred_y=model.predict(X_test)
+
+cm = metrics.confusion_matrix(y_test, pred_y)
+plt.figure(figsize=(9,9))
+sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
+plt.ylabel('Actual label');
+plt.xlabel('Predicted label');
+all_sample_title = 'Naive Bayes Confusion Matrix - score:'+str(metrics.accuracy_score(y_test,pred_y))
+plt.title(all_sample_title, size = 15);
+plt.show()
+print(metrics.classification_report(y_test,pred_y))
+
+
+
+
+
+
